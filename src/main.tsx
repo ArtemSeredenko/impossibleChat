aff279d5f531f49d3d65235a966c172322ccad74
ArtemSeredenko <mpdart2013@gmail.com>
2025-08-25 16:36:39 +0300

    dell app

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+
 import ChatBot from './components/ChatBot.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+  
     <ChatBot />
   </StrictMode>,
 )
